adda03b7f403560127672c2c773b86e34c1a5666
jiralp <69764409+jiralp@users.noreply.github.com>
2020-09-15 19:16:33 +0200

    Help command added

--- cogs/misc.py
+++ cogs/misc.py
@@ -16,6 +16,26 @@ class Misc(commands.Cog):
         await msg.add_reaction("❎")
         await ctx.send(f"{ctx.author.mention}, Suggestion logged!")
 
+    @commands.command()
+    async def help(self, ctx)
+        author = ctx.message.author
+
+        embed = discord.Embed(
+            colour=discord.colour.blue(),
+            title="**Help**"
+            description="Commands for OmniReddit"
+        )
+
+        embed.set_image(url="https://cdn.discordapp.com/attachments/755128550813728815/755137589568078004/Screenshot_20200914_204646.jpg")
+        embed.add_field(name="**Reddit Commands**")
+        embed.add_field(name="search", value="Searches for a post")
+        embed.add_field(name="subreddit", value="Shows the subreddit most recent posts")
+        embed.add_field()
+        embed.add_field(name="**Misc**")
+        embed.add_field(name="Suggest", value="Sends a suggestion to a channel")
+
+        await author.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Misc(bot))
